23cfdb7d5b0ef5a65e34a78cacc063d09619d904
Chixi <chixiisreallynotme@users.noreply.github.com>
2026-07-07 00:03:10 +0200

    chore: remove test folder from repository

--- src/test/java/fr/chixi/cubeconquest/TestEntity.java
+++
@@ -1,4 +0,0 @@
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.item.ItemEntity;
-public class TestEntity {}
--- src/test/java/fr/chixi/cubeconquest/TestEntityLoad.java
+++
@@ -1,6 +0,0 @@
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-public class TestEntityLoad {
-    static {
-        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {});
-    }
-}
